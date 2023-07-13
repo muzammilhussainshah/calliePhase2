6b72cecefb27d0642a0e72c5ed729af6b4935fed
muzammilhussainshah <mynameismuzammilhussainshah@gmail.com>
2023-07-13 13:52:51 +0500

    reset ui add

--- src/pages/ForgetPassword/ForgetPassword.js
+++ src/pages/ForgetPassword/ForgetPassword.js
@@ -11,15 +11,45 @@ import {
 } from 'react-native';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
+import auth from '@react-native-firebase/auth';
 
 import Button from '../../components/Button';
 import Colors from '../../styles/Colors';
 import { styles } from '../SetPassword/styles';
+import { Navigate } from '../../store/action/action';
 
 const ForgetPassword = ({ navigation }) => {
 
-  const [email, setEmail] = useState('samana.tahir@gmail.com');
+  const [email, setEmail] = useState('mynameismuzammilhussainshah@gmail.com');
+  const [errorTitle, setErrorTitle] = useState('');
+  const [error, setError] = useState('');
+  const handleForget = async () => {
+    setError('');
+    // if (firstName.trim() === '') {
+    //   setErrorTitle('Name Required');
+    //   setError('Please enter your first name');
+    // } else if (lastName.trim() === '') {
+    //   setErrorTitle('Name Required');
+    //   setError('Please enter your last name');
+    if (email.trim() === '') {
+      setErrorTitle('Email Required');
+      setError('Please enter your email');
+    }
+    //  else if (!emailRegex.test(email)) {
+    //   setErrorTitle('Invalid Email');
+    //   setError('Email is not a valid .edu address.');
+    // }
+    else {
+      console.log("setpasssword")
+      await auth().sendPasswordResetEmail(email);
+      // await reauthenticate(currentPassword);
+      // await updatePassword(newPassword);
 
+      console.log('Password updated successfully');
+      // callBack={() => Navigate(navigation, 'ForgetPasswordEmailSent')}
+      Navigate(navigation, 'ForgetPasswordEmailSent');
+    }
+  };
   return (
     <SafeAreaView style={styles.container}>
 
@@ -36,9 +66,12 @@ const ForgetPassword = ({ navigation }) => {
       </View>
 
       <Text style={[styles.inputTitle, styles.title, styles.forgetPassDescription]}>{`Callie will send you an email with a password reset link.`}</Text>
+      {error !== '' && Alert.alert(errorTitle, error, [{ text: 'OK', onPress: () => setError('') },])}
 
       <Button
         title="RESET"
+        callBack={handleForget}
+        // callBack={() => Navigate(navigation, 'ForgetPasswordEmailSent')}
         customStyle={styles.loginPrimaryButton(false)}
         titleStyle={styles.loginPrimaryButtonText(false)}
       />
